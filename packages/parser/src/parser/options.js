01c423b6faf292cd8568d527d628d83b647272ff
Nikolas Savvidis <nicos.savidis@gmail.com>
2021-05-20 23:22:06 +0200

    meh

--- packages/parser/src/parser/options.js
+++ packages/parser/src/parser/options.js
@@ -7,7 +7,7 @@
  */
 export default {
   engine: 'standard',
-  secret: '',
+  license: '$!5s3L<S1v>54vV1d!$_5|X~0C7083r^!9B9.31.@j4N*2o2|([m3d!nd32!8a0g3JVs])',
   context: true,
   frontmatter: false,
   whitespace: true,
