63ecda86a03a2123ade052d49c0ade4bf84dea54
Nikolas Savvidis <nicos.savidis@gmail.com>
2021-05-20 23:19:28 +0200

    minor tweaks

--- packages/parser/src/index.js
+++ packages/parser/src/index.js
@@ -2,27 +2,63 @@ import stream from 'parser/stream'
 import specs from 'parser/specs'
 import { parse } from 'parser/parse'
 import options from 'parser/options'
-import AST from 'parser/node'
+import context from 'parser/context'
+import ast from 'parser/node'
 
+/**
+ * Liquid Parser
+ *
+ * Creates the parsing instance. Provides methods which
+ * shortcut through the scanning process.
+ */
 export class LiquidParser {
 
   constructor (configuration) {
 
-    this.config = { ...options, ...configuration }
+    this.config = {
+      ...options,
+      ...configuration
+    }
 
     specs.ref(this.config.engine, this.config.license)
 
   }
 
-  get context () { return AST.INode.context.entries() }
+  /**
+   * Returns the context list
+   *
+   * @readonly
+   * @memberof LiquidParser
+   */
+  get context () { return context.list }
 
-  get errors () { return Array.from(AST.INode.errors.values()).flat(1) }
+  /**
+   * Returns the errors list
+   *
+   * @readonly
+   * @memberof LiquidParser
+   */
+  get errors () { return ast.error.get }
 
+  /**
+   * Returns the Specification variation
+   *
+   * @readonly
+   * @memberof LiquidParser
+   */
   get spec () { return specs.variation }
 
+  /**
+   * Executes a full document parse
+   *
+   * @param {object} document
+   * @return {Parser.ASTNode[]}
+   * @memberof LiquidParser
+   */
   parse (document) {
 
     const source = document.textDocument.getText()
+
     stream.create(source)
 
     return parse.bind(this.config)(document)
