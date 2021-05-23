e792edcbc68bf9fc5975fd9c2e3be2e73dfdf9f3
Nikolas Savvidis <nicos.savidis@gmail.com>
2021-05-23 18:13:43 +0200

    remove types that are supplied in different packages

--- packages/server/types/config.d.ts
+++
@@ -1,7 +0,0 @@
-import { Specification } from "./specification";
-
-export interface Config {
-  engine: "standard" | "shopify" | "jekyll";
-  specification: Specification;
-  strictSpec: boolean;
-}
