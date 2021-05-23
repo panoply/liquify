9a17efe0fa905f4562da6c1c975a1c2d9f2b4197
Nikolas Savvidis <nicos.savidis@gmail.com>
2021-05-23 18:12:51 +0200

    remove validations from previous build
    We now use the parser to run all forms a validations

--- packages/server/src/service/validations/index.js
+++
@@ -1,4 +0,0 @@
-export { default as object } from './object'
-export { default as control } from './control'
-// export { default as iteration } from './iteration'
-export { default as filter } from './filter'
