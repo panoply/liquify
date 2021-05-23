4b8f9c2922e547cb8e7bddc2e8b0a6d15e6df745
Nikolas Savvidis <nicos.savidis@gmail.com>
2021-05-23 18:16:25 +0200

    use alias and adjust build config

--- packages/server/rollup.config.js
+++ packages/server/rollup.config.js
@@ -3,12 +3,13 @@ import { terser } from 'rollup-plugin-terser'
 import json from '@rollup/plugin-json'
 import babel from '@rollup/plugin-babel'
 import commonjs from '@rollup/plugin-commonjs'
-import replace from '@rollup/plugin-replace'
+import alias from '@rollup/plugin-alias'
+import { resolve } from 'path'
+// import replace from '@rollup/plugin-replace'
 import filesize from 'rollup-plugin-filesize'
 import obfuscator from '@liquify/rollup-plugin-obfuscator'
 import noderesolve from '@rollup/plugin-node-resolve'
 import globs from '@liquify/rollup-plugin-globs'
-import * as Lexical from './src/parser/lexical/liquid'
 import pkg from './package.json'
 
 export default {
@@ -16,12 +17,8 @@ export default {
   output: {
     file: 'package/server.js',
     format: 'cjs',
-    sourcemap: !process.env.prod
-    /* banner: banner({
-      ...pkg
-      // main: 'package/server.js'
-    }) */
-
+    sourcemap: !process.env.prod,
+    banner: banner(pkg, 'CC BY-NC-ND 4.0')
   },
   external: [
     'lodash',
@@ -37,68 +34,89 @@ export default {
     'fs',
     'path'
   ],
-  plugins: plugins([
-    json({
-      preferConst: true,
-      compact: !!process.env.prod
-    }),
-
-    babel({
-      babelHelpers: 'runtime',
-      configFile: './.babelrc'
-    }),
-    commonjs(),
-    globs({
-      globs: [
-        'package.json',
-        'readme.md',
-        'changelog.md',
-        'ThirdPartyNotices.txt',
-        'LICENSE',
-        'shopify-sections.json'
-      ],
-      dest: 'package',
-      transform: {
-        LICENSE: '[name].txt',
-        '*.json': ({ content }) => ({
-          content: jsonmin(content.toString())
-        })
-      }
-    })
-  ],
-  [
-    noderesolve(),
-    terser({
-      ecma: 6
-      , warnings: 'verbose'
-      , compress: { passes: 2 }
-    }),
-    obfuscator({
-      target: 'node',
-      compact: true,
-      controlFlowFlattening: false,
-      deadCodeInjection: false,
-      debugProtection: false,
-      debugProtectionInterval: false,
-      disableConsoleOutput: false,
-      identifierNamesGenerator: 'hexadecimal',
-      log: true,
-      renameGlobals: false,
-      rotateStringArray: true,
-      selfDefending: true,
-      shuffleStringArray: true,
-      splitStrings: false,
-      sourceMap: true,
-      stringArray: true,
-      stringArrayEncoding: false,
-      stringArrayThreshold: 0.75,
-      unicodeEscapeSequence: false
-    }),
-    filesize({
-      showGzippedSize: false,
-      showMinifiedSize: true
-    })
-  ])
+  plugins: plugins(
+    [
+      alias(
+        {
+          entries: {
+            provide: resolve(__dirname, 'src/provide'),
+            service: resolve(__dirname, 'src/service'),
+            modes: resolve(__dirname, 'src/service/modes'),
+            utils: resolve(__dirname, 'src/utils')
+          }
+        }
+      ),
+      json(
+        {
+          preferConst: true,
+          compact: !!process.env.prod
+        }
+      ),
+      babel(
+        {
+          babelHelpers: 'runtime',
+          configFile: './.babelrc'
+        }
+      ),
+      commonjs(),
+      globs(
+        {
+          globs: [
+            'package.json',
+            'readme.md',
+            'changelog.md',
+            'ThirdPartyNotices.txt',
+            'LICENSE',
+            'shopify-sections.json'
+          ],
+          dest: 'package',
+          transform: {
+            LICENSE: '[name].txt',
+            '*.json': ({ content }) => ({
+              content: jsonmin(content.toString())
+            })
+          }
+        }
+      )
+    ],
+    [
+      noderesolve(),
+      terser(
+        {
+          ecma: 2016,
+          compress: { passes: 2 }
+        }
+      ),
+      obfuscator(
+        {
+          target: 'node',
+          compact: true,
+          controlFlowFlattening: false,
+          deadCodeInjection: false,
+          debugProtection: false,
+          debugProtectionInterval: false,
+          disableConsoleOutput: false,
+          identifierNamesGenerator: 'hexadecimal',
+          log: true,
+          renameGlobals: false,
+          rotateStringArray: true,
+          selfDefending: true,
+          shuffleStringArray: true,
+          splitStrings: false,
+          sourceMap: true,
+          stringArray: true,
+          stringArrayThreshold: 0.75,
+          unicodeEscapeSequence: false
+        }
+      ),
+      filesize(
+        {
+          showGzippedSize: false,
+          showMinifiedSize: true
+        }
+      )
+    ]
+  )
 }
 /* {
     input: p('node_modules/prettydiff/js/prettydiff.js'),
