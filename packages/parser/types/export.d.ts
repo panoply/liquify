74c2aa9b5abb85cf8392a7ab225901c1cc2f322e
Nikolas Savvidis <nicos.savidis@gmail.com>
2021-05-26 05:16:15 +0200

    Update export.d.ts

--- packages/parser/types/export.d.ts
+++ packages/parser/types/export.d.ts
@@ -1,11 +1,16 @@
-import { Context, IParseError, Variation } from "./index";
+import { Context, IParseError, Variation, Nodes } from "./index";
+import { ASTNode } from "./ast";
 import { Scope } from "./vscode";
+import { Options } from "./options";
 
 export class LiquidParser {
+  constructor(options: Options);
   engine(engine: Specs.Engine): void;
   get context(): Context;
   get errors(): IParseError[];
   get spec(): Variation;
+  get embedded(): ASTNode[];
   parse(scope: Scope): Scope;
+  getEmbeds(document: Scope): ASTNode[];
   // increment (param, contentChanges) {}
 }
