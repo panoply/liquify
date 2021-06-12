7edb9ec6a2b1ed70086cbae6159288ffa0f372cf
Nikolas Savvidis <nicos.savidis@gmail.com>
2021-06-12 13:12:56 +0200

    likely to boycott

---
+++ packages/server/src/export.ts
@@ -0,0 +1,14 @@
+// @ts-ignore
+
+import { LiquidServer } from './provide/server';
+import { LiquidService } from './provide/service';
+
+/**
+ * Language Server
+ */
+export const Server = new LiquidServer();
+
+/**
+ * Language Services
+ */
+export const Service = new LiquidService();
