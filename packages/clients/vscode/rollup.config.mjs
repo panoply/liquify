319fa2ac0d167c1ed9baf67124cec35774b1adf0
Nikolas Savvidis <nicos.savidis@gmail.com>
2021-06-16 21:21:10 +0200

    use rollup export defineConfig

--- packages/clients/vscode/rollup.config.mjs
+++ packages/clients/vscode/rollup.config.mjs
@@ -1,3 +1,4 @@
+import { defineConfig as Rollup } from 'rollup';
 import { terser } from 'rollup-plugin-terser';
 import typescript from 'rollup-plugin-typescript2';
 import { env, jsonmin, config, banner } from '@liquify/rollup-plugin-utils';
@@ -9,106 +10,108 @@ import del from 'rollup-plugin-delete';
 import { path } from '@liquify/schema-stores';
 import filesize from 'rollup-plugin-filesize';
 
-export default {
-  input: 'src/index.ts',
-  output: [
-    {
-      format: 'cjs',
-      file: config.output.cjs,
-      sourcemap: process.env.prod ? false : 'inline',
-      exports: 'named',
-      esModule: false,
-      preferConst: true,
-      banner: banner('PROPRIETARY')
-    },
-    {
-      format: 'es',
-      file: config.output.esm,
-      sourcemap: process.env.prod ? false : 'inline',
-      esModule: false,
-      preferConst: true,
-      banner: banner('PROPRIETARY')
-    }
-  ],
-  external: [
-    'vscode',
-    'vscode-languageclient',
-    'vscode-languageclient/node',
-    'path',
-    'fs'
-  ],
-  plugins: env.unless(process.env.prod)(
-    [
-      replace(
-        {
-          preventAssignment: true,
-          values: {
-            'process.env.SERVER_PATH': '"./../"'
-          }
-        }
-      ),
-      del(
-        {
-          verbose: true,
-          runOnce: !process.env.prod,
-          targets: 'package/*'
-        }
-      ),
-      watch(
-        {
-          assets: [
-            'src/syntax/**/*.json'
-          ]
-        }
-      ),
-      copy(
-        {
-          verbose: true,
-          copyOnce: !!process.env.prod,
-          targets: [
-            {
-              cwd: process.cwd(),
-              src: path(
-                [
-                  'liquidrc',
-                  'specs',
-                  'shopify/settings_data',
-                  'shopify/settings_schema'
-                ]
-              ),
-              dest: 'package/@stores'
-            },
-            {
-              src: 'syntax/**/*.json',
-              dest: 'package/syntax',
-              transform: contents => jsonmin(contents.toString())
+export default Rollup(
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        format: 'cjs',
+        file: config.output.cjs,
+        sourcemap: env.is('dev', 'inline'),
+        exports: 'named',
+        esModule: false,
+        preferConst: true,
+        banner: banner('PROPRIETARY')
+      },
+      {
+        format: 'es',
+        file: config.output.esm,
+        sourcemap: env.is('dev', 'inline'),
+        esModule: false,
+        preferConst: true,
+        banner: banner('PROPRIETARY')
+      }
+    ],
+    external: [
+      'vscode',
+      'vscode-languageclient',
+      'vscode-languageclient/node',
+      'path',
+      'fs'
+    ],
+    plugins: env.if('dev')(
+      [
+        replace(
+          {
+            preventAssignment: true,
+            values: {
+              'process.env.SERVER_PATH': '"./../"'
             }
+          }
+        ),
+        del(
+          {
+            verbose: true,
+            runOnce: env.watch,
+            targets: 'package/*'
+          }
+        ),
+        watch(
+          {
+            assets: [
+              'src/syntax/**/*.json'
+            ]
+          }
+        ),
+        copy(
+          {
+            verbose: true,
+            copyOnce: env.watch,
+            targets: [
+              {
+                cwd: process.cwd(),
+                src: path(
+                  [
+                    'liquidrc',
+                    'specs',
+                    'shopify/settings_data',
+                    'shopify/settings_schema'
+                  ]
+                ),
+                dest: 'package/@stores'
+              },
+              {
+                src: 'syntax/**/*.json',
+                dest: 'package/syntax',
+                transform: contents => jsonmin(contents.toString())
+              }
 
-          ]
-        }
-      ),
-      typescript(
-        {
-          useTsconfigDeclarationDir: true
-        }
-      ),
-      beep()
-    ]
-  )(
-    [
-      terser(
-        {
-          ecma: 2016
-          , warnings: 'verbose'
-          , compress: { passes: 2 }
-        }
-      ),
-      filesize(
-        {
-          showGzippedSize: false,
-          showMinifiedSize: true
-        }
-      )
-    ]
-  )
-};
+            ]
+          }
+        ),
+        typescript(
+          {
+            useTsconfigDeclarationDir: true
+          }
+        ),
+        beep()
+      ]
+    )(
+      [
+        terser(
+          {
+            ecma: 2016
+            , warnings: 'verbose'
+            , compress: { passes: 2 }
+          }
+        ),
+        filesize(
+          {
+            showGzippedSize: false,
+            showMinifiedSize: true
+          }
+        )
+      ]
+    )
+  }
+);
