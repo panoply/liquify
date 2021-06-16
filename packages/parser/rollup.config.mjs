f946bbe654ec5133b08ecf81df933ca571bea9cc
Nikolas Savvidis <nicos.savidis@gmail.com>
2021-06-16 21:21:55 +0200

    incremental
    the ts compiler is fucking slow.

--- packages/parser/rollup.config.mjs
+++ packages/parser/rollup.config.mjs
@@ -1,90 +1,99 @@
+import { defineConfig as Rollup } from 'rollup';
 import { env, config } from '@liquify/rollup-plugin-utils';
 import alias from '@rollup/plugin-alias';
-import typescript from 'rollup-plugin-typescript2';
+import ts from 'rollup-plugin-typescript2';
+import typescript from 'typescript';
+
 import { terser } from 'rollup-plugin-terser';
 import commonjs from '@rollup/plugin-commonjs';
 import filesize from 'rollup-plugin-filesize';
 import noderesolve from '@rollup/plugin-node-resolve';
 import beep from '@rollup/plugin-beep';
 import del from 'rollup-plugin-delete';
+import copy from 'rollup-plugin-copy';
 
-export default {
-  input: 'src/index.ts',
-  output: [
-    {
+export default Rollup(
+  {
+    input: 'src/index.ts',
+    output: {
       format: 'cjs',
       file: config.output.cjs,
-      sourcemap: process.env.prod ? false : 'inline',
+      sourcemap: env.is('dev', 'inline'),
       preferConst: true,
       esModule: false,
       chunkFileNames: '[name].js'
     },
-    {
-      format: 'es',
-      file: config.output.esm,
-      sourcemap: process.env.prod ? false : 'inline',
-      preferConst: true,
-      esModule: false,
-      chunkFileNames: '[name].js'
-    }
-  ],
-  external: [
-    '@liquify/liquid-language-specs'
-  ],
-
-  plugins: env.unless(process.env.prod)(
-    [
-      alias({
-        entries: {
-          tree: config.path('src/tree'),
-          parser: config.path('src/parser'),
-          lexical: config.path('src/lexical'),
-          config: config.path('src/config.ts')
-        }
-      }),
-      del(
-        {
-          verbose: true,
-          runOnce: !process.env.prod,
-          targets: 'package/*'
-        }
-      ),
-      typescript(
-        {
-          useTsconfigDeclarationDir: true
-        }
-      ),
-      noderesolve(
-        {
-          extensions: [
-            '.ts',
-            '.js'
-          ]
-        }
-      ),
-      commonjs(
-        {
-          requireReturnsDefault: 'namespace'
-        }
-      ),
-      beep()
-    ]
-  )(
-    [
-      terser(
-        {
-          ecma: 2016,
-          compress: {
-            passes: 5
+    external: [
+      '@liquify/liquid-language-specs'
+    ],
+    plugins: env.if('dev')(
+      [
+        alias({
+          entries: {
+            tree: config.path('src/tree'),
+            parser: config.path('src/parser'),
+            lexical: config.path('src/lexical'),
+            config: config.path('src/config.ts')
           }
-        }
-      ),
-      filesize(
-        {
-          showGzippedSize: false,
-          showMinifiedSize: true
-        }
-      )
-    ]
-  )
-};
+        }),
+        del(
+          {
+            verbose: true,
+            runOnce: env.watch,
+            targets: 'package/*'
+          }
+        ),
+        copy(
+          {
+            verbose: true,
+            copyOnce: env.watch,
+            targets: [
+              {
+                src: 'node_modules/@liquify/liquid-language-specs/package/@specs',
+                dest: 'package/@specs'
+              }
+            ]
+          }
+        ),
+        ts(
+          {
+            check: false,
+            useTsconfigDeclarationDir: true,
+            typescript
+          }
+        ),
+        noderesolve(
+          {
+            extensions: [
+              '.ts',
+              '.js'
+            ]
+          }
+        ),
+        commonjs(
+          {
+            requireReturnsDefault: 'namespace'
+          }
+        ),
+        beep()
+      ]
+    )(
+      [
+        terser(
+          {
+            ecma: 2016,
+            compress: {
+              passes: 5
+            }
+          }
+        ),
+        filesize(
+          {
+            showGzippedSize: false,
+            showMinifiedSize: true
+          }
+        )
+      ]
+    )
+  }
+);
