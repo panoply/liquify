227271bff4afae1967271227b0d56055079614af
Nikolas Savvidis <nicos.savidis@gmail.com>
2021-07-12 00:33:17 +0200

    Create enum range type export
    Enum range ares fed to the interfaces so a single Type export can be used opposed to the current approach of multiple depth Types using namespace.

--- packages/specs/src/liquid/types/utils.ts
+++ packages/specs/src/liquid/types/utils.ts
@@ -38,3 +38,23 @@ export type PickByValue<T, V> = Pick<T, {
 export type Entries<T> = {
   [K in keyof T]: [keyof PickByValue<T, T[K]>, T[K]];
 }[keyof T][];
+
+/* ENUM VALUES -------------------------------- */
+
+type PrependNextNum<A extends Array<unknown>> = A['length'] extends infer T
+  ? ((t: T, ...a: A) => void) extends (
+    (...x: infer X) => void)
+  ? X : never : never;
+
+type EnumerateInternal<A extends Array<unknown>, N extends number> = {
+  0: A, 1:
+  EnumerateInternal<PrependNextNum<A>, N>
+}[N extends A['length'] ? 0 : 1];
+
+export type Enumerate<N extends number> = EnumerateInternal<[], N> extends (infer E)[]
+  ? E
+  : never;
+
+export type Range<
+  FROM extends number, TO extends number
+> = Exclude<Enumerate<TO>, Enumerate<FROM>>;
