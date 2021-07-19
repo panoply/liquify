45cd302a36bce37451206bc15e86ceb9846db9c0
Nikolas Savvidis <nicos.savidis@gmail.com>
2021-07-19 19:35:10 +0200

    configure specs generation for HTML5 language

--- packages/specs/scripts/html-data.js
+++ packages/specs/scripts/html-data.js
@@ -8,6 +8,25 @@ const toJavaScript = obj => JSON.stringify(obj, null, 2);
 
 const DATA_TYPE = 'IHTMLSpecs';
 
+const VOIDS = [
+  'area',
+  'base',
+  'br',
+  'col',
+  'embed',
+  'hr',
+  'img',
+  'input',
+  'keygen',
+  'link',
+  'menuitem',
+  'meta',
+  'param',
+  'source',
+  'track',
+  'wbr'
+];
+
 const tags = data.tags.reduce((
   acc,
   {
@@ -20,7 +39,33 @@ const tags = data.tags.reduce((
   ...acc,
   [name]: {
     description: value,
-    attributes,
+    void: VOIDS.includes(name),
+    attributes: attributes.length > 0
+      ? attributes.reduce((
+        attr,
+        {
+          name,
+          description,
+          valueSet
+        }
+      ) => {
+
+        const prop = { name };
+
+        if (typeof description === 'object') {
+          prop.description = description.value;
+        } else if (typeof description === 'string') {
+          prop.description = description;
+        }
+
+        if (valueSet) prop.value = valueSet;
+
+        attr.push(prop);
+
+        return attr;
+
+      }, []) : [],
+
     reference: ref
   }
 }), {});
@@ -39,7 +84,7 @@ const attrs = data.globalAttributes.reduce((
   acc[name] = {};
 
   if (attributes) acc[name].attributes = attributes;
-  if (valueSet) acc[name].valueSet = valueSet;
+  if (valueSet) acc[name].value = valueSet;
   if (description) acc[name].description = description.value;
   if (references) acc[name].reference = { name: references[0].name, url: references[0].url };
 
@@ -53,10 +98,23 @@ const valueSets = data.valueSets.reduce((
     name,
     values
   }
-) => ({
-  ...acc,
-  [name]: values
-}), {});
+) => {
+
+  return {
+    ...acc,
+    [name]: values.map((value) => {
+      if (value.description) {
+        return {
+          label: value.name,
+          documentation: value.description
+        };
+      } else {
+        return { label: value.name };
+      }
+    })
+  };
+
+}, {});
 
 function WriteFiles ([ name, contents ]) {
 
@@ -68,10 +126,16 @@ function WriteFiles ([ name, contents ]) {
 
 }
 
+function imports (name) {
+
+  return `import { ${name} as I${name} } from '../types/markup';\n\nexport let ${name}: I${name};`;
+
+}
+
 [
-  [ 'tags', `export default ${toJavaScript(tags)}` ],
-  [ 'attributes', `export default ${toJavaScript(attrs)}` ],
-  [ 'values', `export default ${toJavaScript(valueSets)}` ]
+  [ 'tags', `${imports('Tags')}\n\nTags = ${toJavaScript(tags)}` ]
+  // [ 'attributes', `${imports('Attributes')}\n\nAttributes = ${toJavaScript(attrs)}` ],
+  // [ 'values', `${imports('Values')}\n\nValues = ${toJavaScript(valueSets)}` ]
 
 ].forEach(WriteFiles);
 
