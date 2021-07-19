7a93cc56d4015c3ca061864a2ba065d1a1987e18
Nikolas Savvidis <nicos.savidis@gmail.com>
2021-07-19 19:32:36 +0200

    modularize exports

---
+++ packages/specs/src/liquid/specs.ts
@@ -0,0 +1,2 @@
+export * from './data/export';
+export { engine, complete } from './controller/queries';
---
+++ packages/specs/src/liquid/state.ts
@@ -0,0 +1,3 @@
+/* eslint-disable object-curly-newline */
+
+export { tag, filter, argument, object, value } from './controller/queries';
