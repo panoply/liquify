e0eef9513cc3675f2891a333d402af8e54188a4a
Nikolas Savvidis <nicos.savidis@gmail.com>
2021-07-19 19:33:51 +0200

    main export

---
+++ packages/specs/src/export.ts
@@ -0,0 +1,11 @@
+
+/* METHOD EXPORTS ----------------------------- */
+
+export * as state from './states';
+export * as query from './shared';
+export * as liquid from './liquid/specs';
+export * as html5 from './html/specs';
+
+/* TYPINGS EXPORTS ---------------------------- */
+export * from './html/index';
+export * from './liquid/index';
