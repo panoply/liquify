22303761aaf26f28b7f1064aba74c628fc9521b3
Nikolas Savvidis <nicos.savidis@gmail.com>
2021-07-21 19:15:16 +0200

    export functions

--- packages/specs/src/html/index.ts
+++ packages/specs/src/html/index.ts
@@ -1,8 +1,23 @@
 /* eslint-disable object-curly-newline */
 
 export {
+  IHTMLSpecs,
   IHTMLAttribute,
   IHTMLTag,
   IHTMLTagAttributes,
   IHTMLValue
 } from './types/markup';
+
+export {
+  IHTMLCompletionAttrs,
+  IHTMLCompletionData,
+  IHTMLCompletionTagData,
+  IHTMLCompletionTags,
+  IHTMLCompletions,
+  IHTMLProvideAttrs
+} from './types/completions';
+
+export {
+  HTMLDataVSCode,
+  DataSource
+} from './types/custom';
