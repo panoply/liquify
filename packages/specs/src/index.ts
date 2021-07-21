f545c5c0d2762e46e1d306814531b44603a77e1c
Nikolas Savvidis <nicos.savidis@gmail.com>
2021-07-21 19:16:21 +0200

    adjust exports

--- packages/specs/src/index.ts
+++ packages/specs/src/index.ts
@@ -1,6 +1,7 @@
 
 /* METHOD EXPORTS ----------------------------- */
 
+export * as provide from './shared/provide';
 export * as state from './shared/states';
 export * as query from './shared/query';
 export * as liquid from './liquid/specs';
@@ -10,4 +11,7 @@ export * as html5 from './html/specs';
 
 export * from './html/index';
 export * from './liquid/index';
+
+/* SHARED TYPINGS ----------------------------- */
+
 export * from './shared/types';
