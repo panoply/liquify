37a4f8a2fb2d9a1c5adb62d201c9504817645c03
Nikolas Savvidis <nicos.savidis@gmail.com>
2021-07-24 21:02:26 +0200

    support object property completions

--- packages/specs/src/liquid/controller/provide.ts
+++ packages/specs/src/liquid/controller/provide.ts
@@ -1,5 +1,7 @@
-import { ICompletions } from '../types/data';
-// import { IProperties } from 'liquid/types/objects';
+import { ICompletions, TypeNames } from '../types/data';
+import { Properties } from '../types/objects';
+import { Type } from '../types/types';
+import { Entries, PickByValue } from '../types/utils';
 import { descriptive } from './../../utils/generators';
 import { Tokens } from '../../shared/types';
 import { completions, variation } from './queries';
@@ -41,34 +43,49 @@ export function LiquidTagResolve (item: CompletionItem, edits?: TextEdit[]) {
 }
 
 export async function LiquidPropertyComplete (
-  objects: Array<{ [offset: number]: string | number }> | string,
-  offset?: number
+  objects: { [offset: number]: string[] | number },
+  scope: { [tagName: string]: string } | string | string | undefined,
+  offset: number
 ) {
 
-  const { properties } = variation.objects[objects[offset - 1]];
-  const props = Object.entries(properties).map(CompleteProps);
+  // Lets first acquire the object from the node
+  // We decrement the offset value by 1 for the dot or bracket seperator
+  let props: string[] | number = objects[offset - 1];
 
-  return props;
+  // If we are pointing to an array, the node objects property would
+  // equate to "{ 10: [ 'object' ] }" where the value 10 is the offset
+  // We prevent passing to the walk() iterator if we an object name.
+  if (Array.isArray(props) && props.length === 1) {
 
-  return;
-  if (Array.isArray(objects) && objects.length === 1) {
-    const { properties } = variation.objects?.[objects[offset][0]];
-    if (properties) return Object.entries(properties).map(CompleteProps);
+    const properties = variation.objects[props[0] as string]?.properties;
+
+    return properties
+      ? Object.entries(properties).map(ProvideProps)
+      : null;
   }
 
-  return;
+  // If props is not pointing to a number, we cancel
+  if (typeof props !== 'number') return null;
 
-  // if (typeof property !== 'number') return [];
+  // Lets now move to the node objects property which contains the
+  // current object depths, eg: "{ 10: [ 'object', 'prop' ], 20: 10 }"
+  // where our offset is "20" which points to property value "10"
+  props = objects[props] as string[];
 
-  return (function walk (prop, property) {
+  // This is our iterator function, it allows us to
+  // walk the objects contained on the node
+  return (function walk (props: string[], value: Properties) {
 
-    const object = property?.[prop[0]]?.properties;
+    const object = value?.[props[0]]?.properties;
 
-    return prop.length > 1 ? object && walk(prop.slice(1), object) : object
-      ? Object.entries(object).map(CompleteProps)
-      : false;
+    // We check if we have walked to the last property
+    // to which we can then provide properties, if we haven't
+    // we continuall walk the values
+    return props.length > 1
+      ? object && walk(props.slice(1), object)
+      : object ? Object.entries(object).map(ProvideProps) : null;
 
-  }(objects.slice(1), variation.objects[objects[0]].properties));
+  }(props.slice(1), variation.objects[props[0]].properties));
 
 }
 
@@ -78,12 +95,20 @@ export async function LiquidPropertyComplete (
  * Sets the completion items that are passed to the completion resolver.
  * Extracts necessary values from the passed in specification record.
  */
-export function CompleteProps ([ label, { description, snippet = label } ]) {
+export function ProvideProps ([
+  label,
+  {
+    description,
+    type,
+    snippet = label
+  }
+]) {
 
   return {
     label,
     kind: CompletionItemKind.Property,
     insertText: snippet,
+    detail: TypeNames[type],
     insertTextFormat: InsertTextFormat.Snippet,
     documentation: descriptive(description),
     data: {
