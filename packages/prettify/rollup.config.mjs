b6989834222500abc8d91fe5952e2c5e3472ab69
Nikolas Savvidis <nicos.savidis@gmail.com>
2022-03-17 23:58:53 +0100

    use root config and leverage dts for types

--- packages/prettify/rollup.config.mjs
+++ packages/prettify/rollup.config.mjs
@@ -1,60 +1,62 @@
 import { rollup, plugin, env } from '@liquify/rollup-config';
 
 export default rollup(
-  {
-    input: 'src/index.ts',
-    preserveEntrySignatures: 'allow-extension',
-    output: [
-      {
-        format: 'cjs',
-        file: 'package/index.js',
-        sourcemap: process.env.prod ? false : 'inline',
-        esModule: false,
-        freeze: false,
-        preferConst: true,
-        chunkFileNames: '[name].js'
-      },
-      {
+  [
+    {
+      input: 'src/index.ts',
+      preserveEntrySignatures: 'allow-extension',
+      output: [
+        {
+          format: 'cjs',
+          file: 'package/index.js',
+          sourcemap: process.env.prod ? false : 'inline',
+          esModule: false,
+          freeze: false,
+          preferConst: true,
+          chunkFileNames: '[name].js'
+        },
+        {
+          format: 'esm',
+          file: 'package/index.mjs',
+          sourcemap: process.env.prod ? false : 'inline',
+          esModule: true,
+          freeze: false,
+          preferConst: true,
+          chunkFileNames: '[name].js'
+        }
+      ],
+      plugins: env.if('dev')(
+        [
+          plugin.del(
+            {
+              verbose: true,
+              runOnce: !process.env.prod,
+              targets: 'package/*'
+            }
+          ),
+          plugin.esbuild()
+        ]
+      )(
+        [
+          plugin.esminify(),
+          plugin.filesize(
+            {
+              showGzippedSize: false,
+              showMinifiedSize: true
+            }
+          )
+        ]
+      )
+    },
+    {
+      input: 'src/index.ts',
+      output: {
         format: 'esm',
-        file: 'package/index.mjs',
-        sourcemap: process.env.prod ? false : 'inline',
-        esModule: true,
-        freeze: false,
-        preferConst: true,
-        chunkFileNames: '[name].js'
-      }
-    ],
-    plugins: env.if('dev')(
-      [
-        plugin.del(
-          {
-            verbose: true,
-            runOnce: !process.env.prod,
-            targets: 'package/*'
-          }
-        ),
-        plugin.copy(
-          {
-            targets: [
-              {
-                src: 'src/types/**',
-                dest: 'package/types'
-              }
-            ]
-          }
-        ),
-        plugin.esbuild()
+        file: 'package/index.d.ts'
+      },
+      plugins: [
+        plugin.dts()
       ]
-    )(
-      [
-        plugin.esminify(),
-        plugin.filesize(
-          {
-            showGzippedSize: false,
-            showMinifiedSize: true
-          }
-        )
-      ]
-    )
-  }
+    }
+  ]
 );
